9e858105111b9a32afd348c3e0a85dc3ce63436b
luiz <developer.luizcarlos@outlook.com>
2024-07-28 13:17:27 -0300

    project finish

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'my-green': 'hsl(75, 94%, 57%)',
+        'my-white': 'hsl(0, 0%, 100%)',
+        'my-grey-700': 'hsl(0, 0%, 20%)',
+        'my-grey-800': 'hsl(0, 0%, 12%)',
+        'my-grey-900': ' hsl(0, 0%, 8%)'
+      },
+      FontFamily: {
+        "inter": ["Inter", "sans-serif"]
+      }
     },
   },
   plugins: [],
